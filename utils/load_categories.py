11430cf5369da5b3ca67b1346bcaaa1eb80916ec
Angelika Postaremczak <angelikapostar@gmail.com>
2020-01-16 22:46:53 +0100

    Prepend meme category ID at the beginning of each caption for easier training, gather all the vocabulary of the provided dataset

--- utils/load_categories.py
+++ utils/load_categories.py
@@ -8,13 +8,15 @@ def read_categories(database_path: str = DATABASE_PATH,
                     padded_id_length: int = 12) \
         -> Dict[str, str]:
     """
-    :return: Dictionary {category_id: category_name}.
+    :return: Dictionary {category_name: category_id}.
     All IDs have the same length, padded with zeroes if necessary.
     """
     with open(f"{database_path}/{categories_filename}", "r") as f:
         categories = [line.strip().split(",") for line in f]
-        categories = {
-            category_id.zfill(padded_id_length): category_name
-            for category_id, category_name in categories
-        }
+
+    categories = {
+        category_name: category_id.zfill(padded_id_length)
+        for category_id, category_name in categories
+    }
+
     return categories
--- utils/preprocess_text.py
+++ utils/preprocess_text.py
@@ -1,5 +1,6 @@
 import unicodedata
 from functools import reduce
+from nltk.tokenize import word_tokenize
 from pandas import DataFrame
 from string import ascii_letters, digits
 from typing import Dict, List
@@ -17,6 +18,7 @@ def normalize_caption(caption: str,
 
     def char_to_ascii(char: str) -> str:
         """
+        Makes sure that a character is included in the allowed ASCII alphabet.
         """
         normalized = unicodedata.normalize("NFD", char)
         if normalized in allowed_alphabet:
@@ -28,23 +30,47 @@ def normalize_caption(caption: str,
 
 
 def preprocess_captions(captions_db: Dict[str, DataFrame],
+                        category_ids: Dict[str, str],
                         min_caption_len: int = MIN_CAPTION_LENGTH) \
         -> Dict[str, List[str]]:
     """
     Makes a dictionary of all captions within the categories.
     Turns the original caption lowercase and converts it to ASCII,
     filtering out captions shorter than <min_caption_len> characters.
+
+    :param captions_db: Dictionary of pandas data frame with the memes in
+    the format {category_name: pandas_memes}.
+    :param category_ids: Dictionary of category IDs and their translations
+    to readable names: {category_id: category_name}.
+    :param min_caption_len: Minimum length of a caption for it not to be
+    removed during preprocessing.
+
+    :return: Dictionary of memes in the following format:
+    {category_name: "<category ID>  <caption>;"}
     """
     processed_captions = {}
     for category_name, memes in captions_db.items():
+        # Normalize each captions
         category_captions = map(normalize_caption, memes["caption"].tolist())
+
         # Filter out captions of length smaller than min_caption_len
-        processed_captions[category_name] = list(
+        filtered_captions = list(
             filter(
                 lambda caption: len(caption) >= min_caption_len,
                 category_captions
             )
         )
+
+        # Use word tokenizer
+        tokenized_captions = list(map(word_tokenize, filtered_captions))
+
+        # Prepend the category ID to each caption
+        category_id = category_ids[category_name]
+
+        processed_captions[category_name] = list(map(
+            lambda caption_words: [category_id] + caption_words,
+            tokenized_captions
+        ))
 
     return processed_captions
 
@@ -56,3 +82,17 @@ def get_alphabet(caption_db: Dict[str, List[str]]) -> str:
         category_alphabet = reduce(lambda a, b: a.union(b), captions_alphabets)
         alphabet = alphabet.union(category_alphabet)
     return "".join(sorted(alphabet))
+
+
+def get_vocabulary(caption_db: Dict[str, List[List[str]]]) -> List[str]:
+    """
+    :param caption_db: Meme database, already preprocessed, in the format:
+    {category_name: [[words in caption 1], ...]}
+    :return: List of unique words in the meme database, ordered alphabetically.
+    """
+    vocabulary = set()
+    for category_captions in caption_db.values():
+        category_words_sets = list(map(set, category_captions))
+        category_words = reduce(lambda a, b: a.union(b), category_words_sets)
+        vocabulary = vocabulary.union(category_words)
+    return sorted(vocabulary)
