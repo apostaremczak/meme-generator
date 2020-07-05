3a7908f235a5da5e5c877ae84e599b753c519360
Angelika Postaremczak <angelikapostar@gmail.com>
2020-07-05 03:21:45 +0200

    Correct logger name

--- utils/logger.py
+++ utils/logger.py
@@ -4,4 +4,4 @@ import logging
 def get_logger(logger_name: str = "BasicLogger") -> logging.Logger:
     logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
                         level=logging.INFO)
-    return logging.getLogger("ScraperLogger")
+    return logging.getLogger(logger_name)
