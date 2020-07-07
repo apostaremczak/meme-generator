6ac5d3df894b84e5527f0684804ab5fad01e615d
Angelika Postaremczak <angelikapostar@gmail.com>
2020-07-07 12:08:27 +0200

    Remove console logs

--- web_app/static/js/index.js
+++ web_app/static/js/index.js
@@ -1,19 +1,13 @@
 const GeneratorEndpoint = "generate"
 
-document.addEventListener('DOMContentLoaded',  () => {
+document.addEventListener('DOMContentLoaded', () => {
     const categories = document.querySelectorAll(".category-image")
     console.log(categories)
     categories.forEach(category => {
-        category.addEventListener("click", () => {
-            console.log(`Clicked on ${category.id}`)
-
+        category.addEventListener("click", () =>
             fetch(`${GeneratorEndpoint}/${category.id}`)
                 .then(res => res.json())
-                .then(data => {
-                    console.log(data.result_image)
-                    console.log(document.getElementById("result-image"))
-                    document.getElementById("result-image").src = data.result_image
-                })
-        })
+                .then(data => document.getElementById("result-image").src = data.result_image)
+        )
     })
 })
