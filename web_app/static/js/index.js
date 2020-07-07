7782aae736825057a6256437a9b0d9697c3923f7
Tomek <nasum2@gmail.com>
2020-07-08 00:42:43 +0200

    styling + restructurizing dom

--- web_app/static/js/index.js
+++ web_app/static/js/index.js
@@ -1,13 +1,12 @@
 const GeneratorEndpoint = "generate"
 
-document.addEventListener('DOMContentLoaded', () => {
-    const categories = document.querySelectorAll(".category-image")
-    console.log(categories)
-    categories.forEach(category => {
-        category.addEventListener("click", () =>
-            fetch(`${GeneratorEndpoint}/${category.id}`)
-                .then(res => res.json())
-                .then(data => document.getElementById("result-image").src = data.result_image)
-        )
+const categories = document.querySelectorAll(".category-image")
+console.log(categories)
+categories.forEach(category => {
+    category.addEventListener("click", () => {
+        fetch(`${GeneratorEndpoint}/${category.id}`)
+            .then(res => res.json())
+            .then(data => document.getElementById("result-image").src = data.result_image)
+        window.scrollTo({ top: 0, behavior: 'smooth' })
     })
 })
