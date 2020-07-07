a21c7c60e018d9172375edbf6af773f89d50c086
Tomek <nasum2@gmail.com>
2020-07-08 01:04:42 +0200

    loader + style fix for result

--- web_app/static/js/index.js
+++ web_app/static/js/index.js
@@ -1,12 +1,17 @@
 const GeneratorEndpoint = "generate"
 
+const loadingSrc = "../static/images/loader.gif"
+const resultImage = document.getElementById("result-image")
 const categories = document.querySelectorAll(".category-image")
-console.log(categories)
+
+// console.log(categories)
 categories.forEach(category => {
     category.addEventListener("click", () => {
+        resultImage.src = loadingSrc
+
         fetch(`${GeneratorEndpoint}/${category.id}`)
             .then(res => res.json())
-            .then(data => document.getElementById("result-image").src = data.result_image)
+            .then(data => resultImage.src = data.result_image)
         window.scrollTo({ top: 0, behavior: 'smooth' })
     })
 })
